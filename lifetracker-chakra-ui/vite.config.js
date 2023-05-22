2669fae6d082a5c78001add001306e733edffcc0
jamjamgobambam <jamila.khawaja85@gmail.com>
2023-05-22 12:23:28 -0400

    update vite.config

--- lifetracker-chakra-ui/vite.config.js
+++ lifetracker-chakra-ui/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  rollupOptions: {
+    input: {
+      main: './src/index.jsx'
+    }
+  },
   build: {
     outDir: '../lifetracker-simple-api/public',
     emptyOutDir: true
