3b6c4b4ad9b941a781c4fbf8d932475228eb3f1e
Jamila Cocchiola <82185575+jamjamgobambam@users.noreply.github.com>
2023-07-31 19:18:45 -0500

    update port

--- lifetracker-simple-api/config.js
+++ lifetracker-simple-api/config.js
@@ -7,7 +7,6 @@ require("colors")
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev"
 
-const PORT = process.env.PORT || 3001
 const IS_TESTING = process.env.NODE_ENV === "test"
 
 // Use dev database, testing database, or via env var, production database
@@ -26,17 +25,9 @@ function getDatabaseUri() {
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 const BCRYPT_WORK_FACTOR = IS_TESTING ? 1 : 13
 
-console.log("Life Tracker Config:".green)
-console.log("SECRET_KEY:".yellow, SECRET_KEY)
-console.log("PORT:".yellow, PORT.toString())
-console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR)
-console.log("Database:".yellow, getDatabaseUri())
-console.log("---")
-
 module.exports = {
   IS_TESTING,
   SECRET_KEY,
-  PORT,
   BCRYPT_WORK_FACTOR,
   getDatabaseUri,
 }
